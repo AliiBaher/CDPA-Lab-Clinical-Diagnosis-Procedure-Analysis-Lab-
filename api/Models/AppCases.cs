554f22778dcda9a1b670326cf1eb0f937e76bd6c
Ali Baher <ali.shaki.baher@gmail.com>
2025-12-22 19:38:20 +0300

    refactor: Remove gender and date of birth fields from AppCases model

--- api/Models/AppCases.cs
+++ api/Models/AppCases.cs
@@ -6,9 +6,6 @@ namespace Api.Models
 
         public string SubjectCode { get; set; } = "";  // patient identifier
         public Guid? DoctorId { get; set; }  // uuid, assigned doctor (nullable)
-        
-        public string Gender { get; set; } = "";  // character varying(10)
-        public DateTime Dob { get; set; }  // date of birth
 
         public DateTime EpisodeStart { get; set; }  // timestamp
         public DateTime? EpisodeEnd { get; set; }   // nullable timestamp
